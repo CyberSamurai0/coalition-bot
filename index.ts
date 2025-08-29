cd0e061b724eee21851d1301915841db5b3b8863
CyberSamurai0 <colinsaumure@gmail.com>
2025-08-29 12:34:35 -0400

    Setup bot authentication

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 import * as Discord from "discord.js";
+import {warn, info, ok, err, power} from "./log";
+
+const client : Discord.Client = new Discord.Client({
+    intents: []
+});
+
+client.once(Discord.Events.ClientReady, client => {
+    info(`Logged in as ${client.user.tag}: ${client.user.id}`);
+});
+
+client.login(process.env.DISCORD_TOKEN).then(() => {power("Authenticated with Discord")}).catch(e => {err("Failed to authenticate with Discord:"); console.log(e)});
